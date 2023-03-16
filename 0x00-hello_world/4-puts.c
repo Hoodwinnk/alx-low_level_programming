85202cb13f6ee1f8cf8e42a385d309278aad20a7
Hoodwinnk <kitavititus08@gmail.com>
2023-03-16 14:20:42 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,3 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * Return: 0
- */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-
-}
+ * main - A program that p
